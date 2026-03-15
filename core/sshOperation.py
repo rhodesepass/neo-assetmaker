50dbd3ecfc870b6ea88e0de60236a8f1d5418ee9
PULLUP114514 <Persoutsink@outlook.com>
2026-03-16 01:42:20 +0800

    add: 自动获取json列表

--- core/sshOperation.py
+++ core/sshOperation.py
@@ -9,9 +9,12 @@ import logging
 from typing import Callable, Optional
 import threading
 
+logger = logging.getLogger(__name__)
+
+debug = True
 
 def startDrmApp(ssh):
-
+    '''使用脚本启动DRM'''
     # 为  什  么  白  银  要  把  启  动  外  部  程  序  的  功  能  塞  在  s  h  e  l  l  里  🤬  🤬
     scp = SCPClient(ssh.get_transport())
     scp.put(os.path.join(os.getcwd(),"core","scripts","hostStartDrm.sh"), "/root/hostStartDrm.sh")
@@ -22,3 +25,37 @@ def startDrmApp(ssh):
     stdout.channel.recv_exit_status()
     return
 
+
+
+def RefreshRemoteMaterialList(ssh):
+    '''刷新通行证上的素材列表'''
+
+    stdin, stdout, stderr = ssh.exec_command(
+        '''find /assets/'''
+    )
+    fileListCache = stdout.read().decode()
+    FindJsonPath(fileListCache)
+    
+
+
+
+def FindJsonPath(text):
+    '''使用正则匹配 */*.json'''
+
+    pattern = r'/assets/[^/]+/[^/]+\.json'
+    matches = re.findall(pattern, text, flags=re.UNICODE)
+    return matches
+
+if __name__ == "__main__" and debug == True:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(
+        "192.168.137.2",
+        port="22",
+        username="root",
+        password="toor",
+        timeout=10,
+        banner_timeout=10,
+        auth_timeout=10,
+    )
+    RefreshRemoteMaterialList(ssh)
